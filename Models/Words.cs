3cae5e29333ccadda02dac881542b82b652a811a
Thiago Sandre <Thiago Sandre@DESKTOP-1OOIDE8>
2024-10-07 14:56:18 -0400

    Criação da classe Words para a criação das palavras a serem sorteadas para o Jogo da forca, juntamente ao arquivo WordsRepositor onde ele é responsável por criar as palavra e suas dicas e as escolher randomicamente.

---
+++ Models/Words.cs
@@ -0,0 +1,14 @@
+﻿namespace JogoDaForca.Models
+{
+    public class Words
+    {
+        public Words(string Tips, string Text) 
+        { 
+            this.Tips = Tips;
+            this.Text = Text;
+        }
+        public String Tips { get; set; } = string.Empty;
+
+        public String Text { get; set; } = string.Empty;
+    }
+}
---
+++ Repositories/WordsRepository.cs
@@ -0,0 +1,26 @@
+﻿using JogoDaForca.Models;
+
+namespace JogoDaForca.Repositories
+{
+    public class WordsRepository
+    {
+        private List<Words> _words;
+
+        public WordsRepository()
+        {
+            _words = new List<Words>();
+            _words.Add(new Words("Nome", "João"));
+            _words.Add(new Words("Comida", "Banana"));
+            _words.Add(new Words("Cidade", "São Paulo"));
+            _words.Add(new Words("Carro", "Mclares"));
+            _words.Add(new Words("Moto", "H2R"));
+        }
+
+        public Words GetRandomWords()
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, _words.Count);
+            return _words[number];
+        }
+    }
+}
