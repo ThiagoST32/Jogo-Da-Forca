6301f7edc83402f2d9ce2c94c5e5970a6c414b91
Thiago Sandre <Thiago Sandre@DESKTOP-1OOIDE8>
2024-10-07 15:52:12 -0400

    Criação da lógica por trás do jogo na seleção da classe para o inicio do jogo, juntamente com um evento coletando a letra apertada pelo usuario.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,25 @@
-﻿namespace JogoDaForca
+﻿using JogoDaForca.Models;
+using JogoDaForca.Repositories;
+
+namespace JogoDaForca
 {
     public partial class MainPage : ContentPage
     {
+        private Words _word;
         public MainPage()
         {
             InitializeComponent();
+
+            var repository = new WordsRepository();
+            _word = repository.GetRandomWords();
+
+            LblTips.Text = _word.Tips;
+            LblText.Text = new string('_', _word.Text.Length);
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            string letter = ((Button)sender).Text;
         }
     }
 
