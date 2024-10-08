800354cfe0c8ded9417d308ad4d82173ece6f997
Thiago Sandre <Thiago Sandre@DESKTOP-1OOIDE8>
2024-10-08 15:10:59 -0400

    Regiões adicionadas para seprar metedos com base na sua finalidade, assim como também foi refatorado o código para ter melhor desempenho dentro do App.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -13,9 +13,8 @@ namespace JogoDaForca
         {
             InitializeComponent();
             ResetScreen();
-
         }
-
+        #region Verify if finhiser game
         private async void OnButtonClick(object sender, EventArgs e)
         {
             Button button = (Button)sender;
@@ -28,34 +27,39 @@ namespace JogoDaForca
 
             if (positions.Count == 0)
             {
-                _errors++;
-                ((Button)sender).IsEnabled = false;
-                LblErrors.Text = _errors.ToString();
-                ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
-                button.Style = (Style)App.Current.Resources.MergedDictionaries.ElementAt(1)["Fail"];
-
-                if (_errors == 6)
-                {
-                    await DisplayAlert("Perdeu!", "Quer começar um novo jogo?", "OK");
-                    ResetScreen();
-                }
-
+                ErrorHandler(sender, button);
+                await IsGameOver();
                 return;
             }
 
-            foreach (int position in positions)
-            {
-                LblText.Text = LblText.Text.Remove(position, 1).Insert(position, letter);
-                button.Style = (Style)App.Current.Resources.MergedDictionaries.ElementAt(1)["Sucess"];
-            }
+            ReplaceLetter(button, letter, positions);
+            HasWinner();
         }
-
+        #endregion
+        #region Reset Screen - Back Screen to Initial State
         private void ResetScreen()
         {
             ResetErrors();
             ResetVirtualKeyboard();
             GenerateNewWord();
         }
+        private async void HasWinner()
+        {
+            if (!LblText.Text.Contains("_"))
+            {
+                await DisplayAlert("Parabens!", "Você Ganhou o jogo", "Novo jogo!");
+            }
+        }
+        
+        private async Task IsGameOver()
+        {
+            if (_errors == 6)
+            {
+                await DisplayAlert("Perdeu!", "Quer começar um novo jogo?", "OK");
+                ResetScreen();
+            }
+        }
+
 
         private void GenerateNewWord()
         {
@@ -84,9 +88,34 @@ namespace JogoDaForca
         {
             foreach (Button button in horizontal.Children)
             {
-                button.Style = (Style)App.Current.Resources.MergedDictionaries.ElementAt(1)["NormalButton"];
+                button.Style = null;
                 button.IsEnabled = true;
             }
+        }
+        #endregion
+        #region Handler Error
+        private void ReplaceLetter(Button button, string letter, List<int> positions)
+        {
+            foreach (int position in positions)
+            {
+                LblText.Text = LblText.Text.Remove(position, 1).Insert(position, letter);
+                button.Style = (Style)App.Current.Resources.MergedDictionaries.ElementAt(1)["Sucess"];
+            }
+        }
+
+        private void ErrorHandler(object sender, Button button)
+        {
+            _errors++;
+            ((Button)sender).IsEnabled = false;
+            LblErrors.Text = _errors.ToString();
+            ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+            button.Style = (Style)App.Current.Resources.MergedDictionaries.ElementAt(1)["Fail"];
+        }
+        #endregion
+
+        private void ButtonRestartPlay(object sender, EventArgs e)
+        {
+            ResetScreen();
         }
     }
 
